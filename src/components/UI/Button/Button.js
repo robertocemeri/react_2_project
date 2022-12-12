5caec1f2b50fc3834f040d3b1260fcf4501261c1
Roberto Cemeri <robertocemeri29@gmail.com>
2022-12-12 23:46:42 +0100

    errors

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,8 @@
-import React from "react";
-import styles from "./Button.module.css";
+import React from 'react';
 
-const Button = (props) => {
+import styles from './Button.module.css';
+
+const Button = props => {
   return (
     <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
